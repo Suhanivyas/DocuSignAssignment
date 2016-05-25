63205b9681135239d51fb01187ddd6b6fc71037c
Suhani Vyas <suhani.vyas@gmail.com>
2016-05-25 15:58:07 -0700

    Update test.java

--- src/test.java
+++ src/test.java
@@ -11,6 +11,19 @@ public class Test {
  }
  
  public static void main(String[] args) {
-	 callCommandsFunction("HOT 8, 6, 3");
+	 callCommandsFunction("COJHJLD 8, 6, 3, 4, 2, 5, 1, 7");//invalid Temperature
+	 
+	 callCommandsFunction("COLD 8, 6, 3, 4, 2, 5, 1, 7");
+	 
+	 callCommandsFunction("COLD 8,");//invalid commands
+	 
+	 callCommandsFunction("HOT 8, 6, 4, 2, 1, 7");
+	 
+	 callCommandsFunction("HOT 8, 4, 6, 2, 1, 7");
+	 
+	 callCommandsFunction("HOT --=");//invalid command sequence
+
+	 
+	
  }
 }
